f017153096a6d3e98fc6e8ab0a5ee3d4629cefe4
dimenwarper <dimenwarper@cfaf13ac-159d-0410-80bc-d76a6fea7e4a>
2012-03-13 01:08:31 +0000

    Moving rdat2isatab to commands directory
    git-svn-id: https://simtk.org/svn/rdatkit@32 cfaf13ac-159d-0410-80bc-d76a6fea7e4a

---
+++ README.txt
@@ -0,0 +1,11 @@
+The RNA dataset toolkit
+Authors: 
+
+===Installation instructions for python===
+
+cd rdatkit
+python setup.py install
+
+===Installation instructions for MATLAB===
+
+Add rdatkit/matlab_scripts to your MATLAB path
---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(name='RDATkit',
+        version='0.5',
+        description='RNA dataset toolkit',
+        author='Pablo Cordero',
+        author_email='tsuname@stanford.edu',
+        url='http://rmdb.stanford.edu/rdatkit',
+        packages=['likelihood', 'mutate_and_map'],
+        py_modules=['secondary_structure', 'mapping', 'datahandlers', 'rna', 'view', 'ontology'],
+        requires=['xlwt', 'xlrd', 'biopython']
+        )
