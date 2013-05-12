3754fa4aeba13b8c6ecd22669dffed75c6a88908
rhiju <rhiju@cfaf13ac-159d-0410-80bc-d76a6fea7e4a>
2013-05-12 23:35:36 +0000

    further improvement to get_tag -- now handles tag or annotation in either order, and can handle entire data_annotations (cell of cells)
    git-svn-id: https://simtk.org/svn/rdatkit@67 cfaf13ac-159d-0410-80bc-d76a6fea7e4a

--- matlab_scripts/get_tag.m
+++ matlab_scripts/get_tag.m
@@ -1,8 +1,29 @@
-function value = get_tag( tag, annotation )
-% value = get_tag( tag, annotation )
+function values = get_tag( annotations, tag )
+% value = get_tag( annotations, tag )
+% value = get_tag( annotation, tag )
 %
 % for use in parsing data_annotations or annotations cells.
 %
+
+if ischar( annotations ) & iscell( tag ) % user has given in wrong order.
+  tag_tmp = annotations;
+  annotations = tag;
+  tag = tag_tmp;
+end
+assert( ischar( tag ) );
+assert( iscell( annotations ) );
+
+if length( annotations ) > 0 & ischar( annotations{1} )
+  values = get_tag_one_annotation( annotation, tag );
+  return 
+else
+  for i = 1:length( annotations )
+    values{i} = get_tag_one_annotation( annotations{i}, tag );
+  end
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%`
+function value = get_tag_one_annotation( annotation, tag )
 
 vals = find_annotation_tag( annotation, tag );
 if length( vals ) == 0; 
