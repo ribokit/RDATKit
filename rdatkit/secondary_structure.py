11efc699c1aa7f7adb059eb5776f83317ec94b6c
Tsuname Tendou <dimenwarper@gmail.com>
2015-02-03 17:01:53 -0800

    Pushing changes from masamune
    * Added debug mode
    * Added secondary structure algorithm selection for get_energies
    * Added evaluation.py

--- rdatkit/secondary_structure.py
+++ rdatkit/secondary_structure.py
@@ -8,7 +8,7 @@ from random import *
 from rdatkit import settings
 from rdatkit import mapping
 
-
+debug = False
 class SecondaryStructure:
     def __init__(self, dbn=''):
         self.dbn = dbn
@@ -16,17 +16,26 @@ class SecondaryStructure:
     def __len__(self):
         return len(self.dbn)
 
-    def base_pairs(self):
+    def __str__(self):
+        return self.dbn
+    
+    def _get_base_pairs(self, opch, clch):
         stack = []
         bps = []
         for i, s in enumerate(self.dbn):
-            if s == '(':
+            if s == opch:
                 stack.append(i)
-            if s == ')':
+            if s == clch:
                 bps.append((i, stack.pop()))
         return bps
 
-    def base_pair_dict(self):
+    def base_pairs(self):
+        bps = self._get_base_pairs('(',')')
+        bps += self._get_base_pairs('{','}')
+        bps += self._get_base_pairs('[',']')
+        return bps
+    
+    def _get_base_pair_dict(self, opch, clch):
         stack = []
         bps = {}
         for i, s in enumerate(self.dbn):
@@ -38,18 +47,23 @@ class SecondaryStructure:
                 bps[i] = j
         return bps
 
+    def base_pair_dict(self):
+        bps = self._get_base_pair_dict('(', ')')
+        bps.update(self._get_base_pair_dict('{', '}'))
+        bps.update(self._get_base_pair_dict('[', ']'))
+        return bps
 
-    def helices(self):
+    def _get_helices(self, opch, clch):
         stack = []
         helices = []
         currhelix = []
         for i, s in enumerate(self.dbn):
-            if s == '(':
+            if s == opch:
                 stack.append(i)
-            if s == ')':
+            if s == clch:
                 prevbase = stack.pop()
                 if len(currhelix) > 0:
-                    if currhelix[-1][0] - 1 == prevbase:
+                    if currhelix[-1][0] == prevbase + 1 and currhelix[-1][1] == i - 1:
                         currhelix.append((prevbase, i))
                     else:
                         helices.append(currhelix)
@@ -60,8 +74,14 @@ class SecondaryStructure:
                 if len(currhelix) > 0:
                     helices.append(currhelix)
                     currhelix = []
-            if len(currhelix) > 0:
-                helices.append(currhelix)
+        if len(currhelix) > 0:
+            helices.append(currhelix)
+        return helices
+
+    def helices(self):
+        helices = self._get_helices('(',')')
+        helices += self._get_helices('{','}')
+        helices += self._get_helices('[',']')
         return helices
 
 
@@ -82,14 +102,20 @@ class SecondaryStructure:
             ways = 0
             startj = len(junctions)
             for i, jun in enumerate(junctions):
-                if jun[0] - 1 == pos:
+                if len(jun) > 0 and jun[0] - 1 == pos:
                     startj = i
                     break
             for i in range(startj, len(junctions)):
                 ways += 1
-                junction += junctions[i]
+                junction += [x for x in junctions[i] if self.dbn[x] == '.' or self.dbn[x].lower() == 'a']
             newjunctions = junctions[:startj]
             return junction, ways, newjunctions
+
+        def _get_ssregion(start, i):
+            if start < 0:
+                return  []
+            else:
+                return range(start, i)
 
         """
         """
@@ -108,12 +134,14 @@ class SecondaryStructure:
                     sstate = statestack.pop()
                 if prevstate == '.' and sstate == '-':
                     fragments['dangles'].append(range(i))
-                if (prevstate == '.' or prevstate == ')') and sstate != '-':
-                    jlist.append(range(ssregion_start, i))
-                    jstartstack.append(ssregion_start-1)
-                    wstack.append(1)
+                if sstate != '-':
+                    if (prevstate == '.' or prevstate == ')') and sstate != '-':
+                        jlist.append(_get_ssregion(ssregion_start, i))
+                        jstartstack.append(ssregion_start-1)
+                        wstack.append(1)
+                        ssregion_start = -1
                 prevstate = '('
-            if s == '.':
+            if s == '.' or s == 'a':
                 if prevstate != '.':
                     ssregion_start = i
                     statestack.append(prevstate)
@@ -126,18 +154,19 @@ class SecondaryStructure:
                         for w in range(ways):
                             jstartstack.pop()
                         if ways == 1:
-                            fragments['interiorloops'].append(junction + range(ssregion_start, i))
+                            fragments['interiorloops'].append(junction + _get_ssregion(ssregion_start, i))
                         else:
                             key = '%dwayjunctions' % (ways + 1)
                             if key not in fragments:
                                 fragments[key] = []
-                            fragments[key].append(junction + range(ssregion_start, i))
+                            fragments[key].append(junction + _get_ssregion(ssregion_start, i))
                     else:
                         sstate = statestack.pop()
                         if sstate == '(':
-                            fragments['hairpins'].append(range(ssregion_start, i))
+                            fragments['hairpins'].append(_get_ssregion(ssregion_start, i))
                         if sstate == ')':
-                            fragments['bulges'].append(range(ssregion_start, i))
+                            fragments['bulges'].append(_get_ssregion(ssregion_start, i))
+                    ssregion_start = -1
                 if prevstate == ')':
                     if len(jstartstack) > 0 and jstartstack[-1] == prevbase:
                         jstartstack.pop()
@@ -151,17 +180,15 @@ class SecondaryStructure:
                             for w in range(ways):
                                 jstartstack.pop()
                                 if ways == 1:
-                                    fragments['interiorloops'].append(junction + range(ssregion_start, i))
+                                    fragments['interiorloops'].append(junction + _get_ssregion(ssregion_start, i))
                                 else:
                                     key = '%dwayjunctions' % (ways + 1)
                                     if key not in fragments:
                                         fragments[key] = []
-                                    fragments[key].append(junction + range(ssregion_start, i))
+                                    fragments[key].append(junction + _get_ssregion(ssregion_start, i))
+                                ssregion_start = -1
                 prevstate = ')'
         if self.dbn[-1] == '.':
-<<<<<<< HEAD
-            fragments['dangles'].append(range(ssregion_start, len(self.dbn)))
-=======
             fragments['dangles'].append(_get_ssregion(ssregion_start, len(self.dbn)))
         # Eliminate spurious adds for single stranded regions and re-assign aptamer regions
         aptamers = []
@@ -193,7 +220,6 @@ class SecondaryStructure:
         if len(sstrand_region) > 0:
             fragments['sstrand'].append(sstrand_region)
 
->>>>>>> refs/remotes/origin/master
         return fragments
 
 
@@ -214,6 +240,13 @@ class SecondaryStructure:
                             probs[i] = g.pdf(data[i])
         return probs, prod(probs)
 
+
+def removefile(f):
+    if type(f) == str:
+        os.remove(os.path.abspath(f))
+    else:
+        os.remove(os.path.abspath(f.name))
+
 def to_seqfile(sequence, name='placeholder'):
     seqfile = tempfile.NamedTemporaryFile(delete=False)
     seqfile.write(';\n%(name)s\n%(sequence)s1' % {'name':name, 'sequence':sequence})
@@ -228,6 +261,20 @@ def _prepare_ct_and_seq_files(sequence):
     seqfile.close()
     return seqname, ctname
 
+def _prepare_fasta_file(sequence):
+    fastafile = tempfile.NamedTemporaryFile(delete=False)
+    fastafile.write('>bla\n%s\n' % sequence)
+    return fastafile.name
+
+def _get_fasta_structures(fname):
+    structures = []
+    for l in open(fname).readlines():
+        l = l.strip()
+        if l[0] not in  ['.', '(', ')']:
+            continue
+        structures.append(SecondaryStructure(dbn=l.split()[0]))
+    return structures
+
 def _get_dot_structs(ctname, nstructs, unique=False):
     structs = []
     dbns = []
@@ -237,8 +284,8 @@ def _get_dot_structs(ctname, nstructs, unique=False):
         dbnfile.close()
         os.popen(settings.RNA_STRUCTURE_CT2DOT + ' %s %d %s ' % \
              (ctname, i+1, dbnname))
-        print(settings.RNA_STRUCTURE_CT2DOT + ' %s %d %s ' % \
-             (ctname, i+1, dbnname))
+        if debug: print(settings.RNA_STRUCTURE_CT2DOT + ' %s %d %s ' % \
+             (ctname, i+1, dbnname));
         dbn = open(dbnname).readlines()[-1].strip()
         # Append only non trivial structures
         if '(' in dbn:
@@ -248,6 +295,7 @@ def _get_dot_structs(ctname, nstructs, unique=False):
                     structs.append(SecondaryStructure(dbn=dbn))
             else:
                 structs.append(SecondaryStructure(dbn=dbn))
+        removefile(dbnfile)
     return structs
 
 def _to_ct_file(sequence, struct, filename):
@@ -271,14 +319,25 @@ def _to_ct_file(sequence, struct, filename):
 	    f.write('%s %s %s %s %s %s\n' % (i+1, sequence[i], i, i+2, pair, n))
     f.close()
 
+def _to_fasta_file(sequence, structures, fastaname):
+    fastafile = open(fastaname, 'w')
+    if type(structures) == list:
+        for struct in structures:
+            fastafile.write('>bla\n%s\n%s\n' % (sequence, str(struct)))
+    else:
+        fastafile.write('>bla\n%s\n%s\n' % (sequence, structures))
+    fastafile.close()
+
 def get_boltzmann_weight(sequence, structure, algorithm='rnastructure'):
     if algorithm == 'rnastructure':
         seqname, ctname = _prepare_ct_and_seq_files(sequence)
         _to_ct_file(sequence, structure, ctname)
         energy = get_energies(ctname)[0]
         PARTCMD = settings.RNA_STRUCTURE_PARTITION + ' %s %s ' % (seqname, '/dev/null')
-        print PARTCMD
+        if debug: print PARTCMD;
         ensemble_energy = float(os.popen(PARTCMD).read().split('\n')[-1])
+        removefile(seqname)
+        removefile(ctname)
         kT = 0.5905 #TODO Need to generalize/correct this
         weight = exp(-energy/kT)/exp(-ensemble_energy/kT)
     return weight
@@ -287,18 +346,21 @@ def mea_structure(sequence, algorithm='rnastructure', nstructs=1, gamma=1.0, opt
     if algorithm == 'rnastructure':
         seqname, ctname = _prepare_ct_and_seq_files(sequence)
         CMD = settings.RNA_STRUCTURE + '/exe/MaxExpect -g %s --sequence %s %s %s' % (gamma, seqname, ctname, opts)
-        print CMD
+        if debug: print CMD;
         os.popen(CMD)
         structs = _get_dot_structs(ctname, nstructs)
+        removefile(seqname)
+        removefile(ctname)
     if returnct:
         return structs, ctname
     else:
         return structs
 
-def fold(sequence, algorithm='rnastructure', modifier='shape', mapping_data=[], nstructs=1, fold_opts='', bonus2d=False):
+def fold(sequence, algorithm='rnastructure', mapping_data=[], nstructs=1, fold_opts='', bonus2d=False):
     if algorithm == 'rnastructure':
         seqname, ctname = _prepare_ct_and_seq_files(sequence)
         CMD = settings.RNA_STRUCTURE_FOLD + ' %s %s ' % (seqname, ctname)
+        tmp = None
         if len(mapping_data) > 0:
             if bonus2d:
                 tmp = tempfile.NamedTemporaryFile(delete=False)
@@ -306,27 +368,26 @@ def fold(sequence, algorithm='rnastructure', modifier='shape', mapping_data=[], 
                 tmp.close()
                 CMD += '-x %s ' % tmp.name
             else:
-		tmp = tempfile.NamedTemporaryFile(delete=False)
-		if modifier.lower() == 'dms':
-		    for pos in mapping_data.seqpos:
-			s = sequence[pos].lower()
-			if s == 'a' or s == 'c':
-			    tmp.write('%d %.3f\n' % (pos + 1, float(mapping_data[pos])))
-		    CMD += '-dms %s ' % tmp.name
-		elif modifier.lower() == 'cmct':
-		    for pos in mapping_data.seqpos:
-			s = sequence[pos].lower()
-			if s == 'g' or s == 'u':
-			    tmp.write('%d %.3f\n' % (pos + 1, float(mapping_data[pos])))
-		    CMD += '-cmct %s ' % tmp.name
-		else:
-		    CMD += '-sh %s ' % tmp.name
-		    tmp.write(str(mapping_data))
-		tmp.close()
-        CMD += fold_opts
-        print(CMD)
+                tmp = tempfile.NamedTemporaryFile(delete=False)
+                tmp.write(str(mapping_data))
+                tmp.close()
+                CMD += '-sh %s ' % tmp.name
+                CMD += fold_opts
+        if debug: print(CMD);
         os.popen(CMD)
         structs = _get_dot_structs(ctname, nstructs)
+        removefile(seqname)
+        removefile(ctname)
+        if tmp != None:
+            removefile(tmp)
+    if algorithm == 'viennarna':
+        fastaname = _prepare_fasta_file(sequence)
+        CMD = settings.VIENNA_RNA_FOLD + ' < %s > %s.out' % (fastaname, fastaname)
+        if debug: print(CMD);
+        os.popen(CMD)
+        structs = [SecondaryStructure(dbn=l.split()[0].strip()) for l in open(fastaname + '.out').readlines() if l.strip()[0] in ['(', ')', '.']]
+        removefile(fastaname)
+    
     return structs
 
 def partition(sequence, algorithm='rnastructure', mapping_data=None, fold_opts='', bonus2d=False):
@@ -334,38 +395,70 @@ def partition(sequence, algorithm='rnastructure', mapping_data=None, fold_opts='
         seqname, ctname = _prepare_ct_and_seq_files(sequence)
         CMD = settings.RNA_STRUCTURE_PARTITION + ' %s %s ' % (seqname, ctname)
         if mapping_data:
-            tmp = tempfile.NamedTemporaryFile(delete=False)
-	    if bonus2d:
-		savetxt(tmp, mapping_data)
-		tmp.close()
-		CMD += '-x %s ' % tmp.name
-	    else:
-		tmp.write(str(mapping_data))
-		tmp.close()
-		CMD += '-sh %s ' % tmp.name
+            CMD += _get_mapping_data_file(mapping_data, bonus2d=bonus2d)
             CMD += fold_opts
-        print(CMD)
+        if debug: print(CMD);
         os.popen(CMD)
     bppm = loadtxt('bpp.txt')
     for i in range(bppm.shape[0]):
         for j in range(i,bppm.shape[1]):
             if bppm[i,j] != 0:
                 bppm[j,i] = bppm[i,j]
+    removefile(seqname)
+    removefile(ctname)
     return bppm
 
-def get_structure_energies(sequence, structures):
-    energies = []
-    ctfile = tempfile.NamedTemporaryFile(delete=False)
-    ctname = ctfile.name
-    _to_ct_file(sequence, structures, ctname)
-    return get_energies(ctname)
+def _get_mapping_data_file(mapping_data, bonus2d=False):
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    if bonus2d:
+        savetxt(tmp, mapping_data)
+        tmp.close()
+        opt = ' -x %s ' % tmp.name
+    else:
+        tmp.write(str(mapping_data))
+        tmp.close()
+        opt = ' -sh %s ' % tmp.name
+    return opt
 
-def get_energies(ctname):
-    EFN2CMD = settings.RNA_STRUCTURE_ENERGY + ' %s /tmp/energy' % ctname
-    os.popen(EFN2CMD)
-    energyfile = open('/tmp/energy')
-    energies = [float(line.split(' ')[-1]) for line in energyfile.readlines()]
+
+def get_structure_energies(sequence, structures, mapping_data=None, algorithm='rnastructure'):
+    if algorithm == 'rnastructure':
+        energies = []
+        ctfile = tempfile.NamedTemporaryFile(delete=False)
+        ctname = ctfile.name
+        _to_ct_file(sequence, structures, ctname)
+        energies = get_energies(ctname, mapping_data=mapping_data)
+        removefile(ctfile)
+    if algorithm == 'viennarna':
+        fastafile = tempfile.NamedTemporaryFile(delete=False)
+        fastaname = fastafile.name
+        _to_fasta_file(sequence, structures, fastaname)
+        energies = get_energies(fastaname, format='fasta')
+        removefile(fastafile)
     return energies
+
+def get_energies(fname, format='ct', mapping_data=None):
+    if format == 'ct':
+        energyfile = tempfile.NamedTemporaryFile(delete=False)
+        EFN2CMD = settings.RNA_STRUCTURE_ENERGY + ' %s %s' % (fname, energyfile.name)
+        if mapping_data:
+            EFN2CMD += _get_mapping_data_file(mapping_data)
+        os.popen(EFN2CMD)
+        energyfile.seek(0)
+        energies = [float(line.split(' ')[-1]) for line in energyfile.readlines()]
+        removefile(energyfile)
+        return energies
+    if format == 'fasta':
+        RNAEVALCMD = settings.VIENNA_RNA_ENERGY + '< %s > %s.out' % (fname, fname)
+        os.popen(RNAEVALCMD)
+        energies = []
+        for l in open(fname + '.out').readlines():
+            l = l.strip()
+            if l[0] not in  ['.', '(', ')']:
+                continue
+            energies.append(float(l.split()[-1].strip(')(')))
+        removefile(fname + '.out')
+        return energies
 
 def sample(sequence, algorithm='rnastructure', mapping_data=None, nstructs=1000, unique=False, energies=False):
     if algorithm == 'rnastructure':
@@ -374,6 +467,8 @@ def sample(sequence, algorithm='rnastructure', mapping_data=None, nstructs=1000,
         os.popen(CMD)
         structs = _get_dot_structs(ctname, nstructs, unique=unique)
         energies = get_energies(ctname)
+        removefile(seqname)
+        removefile(ctname)
     if energies:
         return structs, energies
     else:
@@ -388,6 +483,17 @@ def subopt(sequence, algorithm='rnastructure', mapping_data=None, fraction=0.05,
         os.popen(CMD)
         structs = _get_dot_structs(ctname, nstructs)
         energies = get_energies(ctname)
+        removefile(seqname)
+        removefile(ctname)
+    if algorithm == 'viennarna':
+        fastaname = _prepare_fasta_file(sequence)
+        CMD = settings.VIENNA_RNA_SUBOPT + ' -p %s < %s > %s.out' % (nstructs, fastaname, fastaname)
+        if debug: print CMD;
+        os.popen(CMD)
+        structs = _get_fasta_structures(fastaname + '.out')
+        energies = get_structure_energies(sequence, structs, algorithm='viennarna')
+        removefile(fastaname)
+        removefile(fastaname + '.out')
     if energies:
         return structs, energies
     return structs
@@ -407,3 +513,28 @@ def random(nstructs, length, nbp):
                 dbnlist[b1] = ')'
         structs.append(SecondaryStructure(dbn=''.join(dbnlist)))
     return structs
+
+def base_pair_fractions_in_structures(reference, structures, factors=None):
+    ref_bp = reference.base_pairs()
+    if factors == None:
+        factors = [1]*len(structures)
+    bpdict = dict([(bp, 0) for bp in ref_bp])
+    for i, s in enumerate(structures):
+        bps = s.base_pairs()
+        for bp in bps:
+            if bp in bpdict:
+                bpdict[bp] = bpdict[bp] + factors[i]
+            if bp[::-1] in bpdict:
+                bpdict[bp[::-1]] = bpdict[bp[::-1]] + factors[i]
+
+    for bp in bpdict.keys():
+        if factors == None:
+            bpdict[bp] *= 100./len(structures)
+        else:
+            bpdict[bp] *= 100.
+    
+    for bp in ref_bp:
+        bpdict[bp[::-1]] = bpdict[bp]
+
+    return bpdict
+
--- rdatkit/settings.py
+++ rdatkit/settings.py
@@ -1,16 +1,19 @@
 import scipy.stats as stats
 
-EXTERNAL_DIR = '/MATLAB_Code'
-RNA_STRUCTURE = EXTERNAL_DIR + 'RNAstructure/'
-RNA_STRUCTURE_FOLD = EXTERNAL_DIR + 'RNAstructure/exe/Fold'
-RNA_STRUCTURE_ALLSUB = EXTERNAL_DIR + 'RNAstructure/exe/AllSub'
-RNA_STRUCTURE_STOCHASTIC = EXTERNAL_DIR + 'RNAstructure/exe/stochastic'
-RNA_STRUCTURE_ENERGY = EXTERNAL_DIR + 'RNAstructure/exe/efn2'
-RNA_STRUCTURE_PARTITION = EXTERNAL_DIR + 'RNAstructure/exe/partition'
-RNA_STRUCTURE_CT2DOT = EXTERNAL_DIR + 'RNAstructure/exe/ct2dot'
-VIENNA_RNA = EXTERNAL_DIR + 'Biers/ViennaRNA-1.8.4/'
-VARNA = '~/Downloads/Lab/VARNAv3-9.jar'
-MAPPING_DATABASE_PATH = ''
+EXTERNAL_DIR = '/home/tsuname/'
+RNA_STRUCTURE = EXTERNAL_DIR + 'RNAstructure/5.5/'
+RNA_STRUCTURE_FOLD = RNA_STRUCTURE + 'exe/Fold'
+RNA_STRUCTURE_ALLSUB = RNA_STRUCTURE + 'exe/AllSub'
+RNA_STRUCTURE_STOCHASTIC = RNA_STRUCTURE + 'exe/stochastic'
+RNA_STRUCTURE_ENERGY = RNA_STRUCTURE + 'exe/efn2'
+RNA_STRUCTURE_PARTITION = RNA_STRUCTURE + 'exe/partition'
+RNA_STRUCTURE_CT2DOT = RNA_STRUCTURE + 'exe/ct2dot'
+VIENNA_RNA = EXTERNAL_DIR + 'ViennaRNA-1.4/'
+VIENNA_RNA_ENERGY = '/usr/bin/RNAeval'
+VIENNA_RNA_FOLD = '/usr/bin/RNAfold'
+VIENNA_RNA_SUBOPT = '/usr/bin/RNAsubopt'
+VARNA = '/home/tsuname/VARNA.jar'
+MAPPING_DATABASE_PATH = '/home/tsuname/Documents/lab/rhiju/rdatkit/likelihood/databases'
 dists = {}
 for k in ['dangles', 'bulges', 'hairpins']:
     dists[k] = stats.gamma
