48352cb74786d69346a9ca249614d0fb4409c784
T47 <t47@stanford.edu>
2015-03-19 20:40:22 -0700

    indent change for rna.py

--- rdatkit/rna.py
+++ rdatkit/rna.py
@@ -7,45 +7,51 @@ from random import choice
 class RNA:
     def __init__(self, sequence=''):
         self.sequence = sequence
-    
+
     def __len__(self):
         return len(self.sequence)
 
     def bootstrap(self, mapping_data, nboot, nsamples=-1, algorithm='rnastructure', bonus2d=False, replacement=False, fold_opts=''):
         print 'Starting bootstrap...'
-	print 'Folding RNA with complete data'
+        print 'Folding RNA with complete data'
         if bonus2d:
-            mappind_data = array(mapping_data)
+            mapping_data = array(mapping_data)
         if nsamples < 0:
-	    nsamples = len(self.sequence)
-        full_bps = secondary_structure.fold(self.sequence, algorithm=algorithm, mapping_data=mapping_data, fold_opts='', bonus2d=bonus2d)[0].base_pairs()
-	bpdict = dict([(bp, 0) for bp in full_bps])
-	for i in range(nboot):
-	    print 'Doing bootstrap iteration %s' % i
+            nsamples = len(self.sequence)
+
+        print len(self.sequence), mapping_data.shape()
+        full_bps = secondary_structure.fold(self.sequence, algorithm=algorithm, mapping_data=mapping_data, fold_opts='', bonus2d=bonus2d)
+        print len(full_bps)
+        print full_bps
+        full_bps = full_bps[0].base_pairs()
+        bpdict = dict([(bp, 0) for bp in full_bps])
+        for i in range(nboot):
+            print 'Doing bootstrap iteration %s' % i
             if bonus2d:
                 md = zeros(mapping_data.shape)
-		nres = mapping_data.shape[0]
-		for x in xrange(nres):
-		    ncontrib = sum(randint(0, nres, nres) ==  x)
-		    md[:,x] = mapping_data[:,x]*ncontrib
+                nres = mapping_data.shape[0]
+                for x in xrange(nres):
+                    ncontrib = sum(randint(0, nres, nres) ==  x)
+                    md[:,x] = mapping_data[:,x]*ncontrib
                 """
                 grid = indices(mapping_data.shape)
                 all_indices = zip(grid[0].ravel(), grid[1].ravel())
                 sampled_indices = [choice(all_indices) for x in xrange(len(all_indices))]
                 for j,k in sampled_indices:
-                    md[j,k] += mapping_data[j,k]
+                md[j,k] += mapping_data[j,k]
                 """
             else:
                 md = mapping_data.sample(nsamples, replacement=replacement)
-	    struct = secondary_structure.fold(self.sequence, algorithm=algorithm, mapping_data=md, fold_opts='', bonus2d=bonus2d)[0]
-	    bps = struct.base_pairs()
-	    for bp in bps:
-		if bp in bpdict:
-		    bpdict[bp] += 1
-		else:
-		    bpdict[bp] = 1
-        for bp in bpdict:
-	    bpdict[bp] *= 100./nboot
-	return bpdict
+            struct = secondary_structure.fold(self.sequence, algorithm=algorithm, mapping_data=md, fold_opts='', bonus2d=bonus2d)[0]
+            bps = struct.base_pairs()
+            for bp in bps:
+                if bp in bpdict:
+                    bpdict[bp] += 1
+                else:
+                    bpdict[bp] = 1
 
-    
+        for bp in bpdict:
+            bpdict[bp] *= 100./nboot
+        return bpdict
+
+
