5109371707d1af65de50c16ae76d86a9a1dfa43d
T47 <t47@stanford.edu>
2015-03-19 21:00:31 -0700

    version to 0.6

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='RDATkit',
-        version='0.5',
+        version='0.6',
         description='RNA dataset toolkit',
         author='Pablo Cordero',
         author_email='tsuname@stanford.edu',
