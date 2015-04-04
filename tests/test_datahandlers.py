e605636a2c6e795be360d456b9c07abc904198d0
Pablo Pablo Sanchez Cordero Gonzalez <tsuname@corn27.stanford.edu>
2015-04-04 11:17:57 -0700

    Added a test case in test_datahandlers for testing save_a_construct

--- tests/test_datahandlers.py
+++ tests/test_datahandlers.py
@@ -1,6 +1,6 @@
-import datahandlers
+from rdatkit import datahandlers
 import sys
-
+import numpy as np
 print 'Creating RDATFile object'
 rdat = datahandlers.RDATFile()
 print 'Loading RDAT file'
@@ -9,3 +9,11 @@ print 'Validating file'
 rdat.validate()
 print 'Converting to isatab'
 isatab = rdat.toISATAB()
+
+data = np.zeros([2,2])
+data[0,:] = 1
+data_annotations = [{'annotation':['hello'], 'annotation2':'hello2'}, {'annotation':['bla']}]
+sequence ='AAAGGGAAAUUU'
+
+rdat = datahandlers.RDATFile()
+rdat.save_a_construct('test', data, sequence, '...(((...)))', 0, {'yes':'no'}, data_annotations, 'test.rdat', comments='commenting', version=0.32)
